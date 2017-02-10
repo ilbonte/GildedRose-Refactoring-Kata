714a9e6c2e171ca64f433cdf63a374dbd24bde9d
Davide Bontempelli <davide.bonte@gmail.com>
2017-02-10 15:08:03 +0100

    move logic to different method

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -9,53 +9,54 @@ class GildedRose {
 
 	public void updateQuality() {
 		for (int i = 0; i < items.length; i++) {
+			updateItem(items[i]);
+		}
+	}
+	
+	private void updateItem(Item item){
+		if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+			return;
+		}
 
-			if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-				continue;
-			}
-
-			items[i].sellIn -= 1;
-			
-			if (items[i].quality < 50) {
-				if (items[i].name.equals("Aged Brie")) {
-					items[i].quality += 1;
-					continue;
-				}
-
-				if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-
-					if (items[i].sellIn < 0) {
-						items[i].quality = 0;
-						continue;
-					}
-
-					items[i].quality += 1;
-
-					if (items[i].sellIn < 10 && items[i].quality < 50 ) {
-						items[i].quality += 1;
-					}
-
-					if (items[i].sellIn < 5  && items[i].quality < 50) {
-						items[i].quality += 1;
-					}
-
-					continue;
-				}
-				
-				if(items[i].quality>0){
-					items[i].quality -= 1;
-					
-					if(items[i].sellIn<0){
-						items[i].quality -= 1;
-					}
-				}
-				
-				continue;
-				
-				
-			}
-
+		item.sellIn -= 1;
 		
+		if (item.quality < 50) {
+			if (item.name.equals("Aged Brie")) {
+				item.quality += 1;
+				return;
+			}
+
+			if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+
+				if (item.sellIn < 0) {
+					item.quality = 0;
+					return;
+				}
+
+				item.quality += 1;
+
+				if (item.sellIn < 10 && item.quality < 50 ) {
+					item.quality += 1;
+				}
+
+				if (item.sellIn < 5  && item.quality < 50) {
+					item.quality += 1;
+				}
+
+				return;
+			}
+			
+			if(item.quality>0){
+				item.quality -= 1;
+				
+				if(item.sellIn<0){
+					item.quality -= 1;
+				}
+			}
+			
+			return;
+			
+			
 		}
 	}
 }
