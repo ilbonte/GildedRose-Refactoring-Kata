0d1b97ab659a7b768d0743b442ca66251c3fdfd3
Davide Bontempelli <davide.bonte@gmail.com>
2017-02-10 12:22:11 +0100

    remove duplication

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -9,19 +9,21 @@ class GildedRose {
 
 	public void updateQuality() {
 		for (int i = 0; i < items.length; i++) {
+
+			if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+				return;
+			}
+
+			items[i].sellIn -= 1;
+			
 			if (items[i].quality < 50) {
 				if (items[i].name.equals("Aged Brie")) {
-					items[i].quality += 1;
-					items[i].sellIn -= 1;
-					return;
-				}
 
-				if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+					items[i].quality += 1;
 					return;
 				}
 
 				if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-					items[i].sellIn -= 1;
 
 					if (items[i].sellIn < 0) {
 						items[i].quality = 0;
@@ -39,7 +41,6 @@ class GildedRose {
 					}
 
 					return;
-
 				}
 			}
 
