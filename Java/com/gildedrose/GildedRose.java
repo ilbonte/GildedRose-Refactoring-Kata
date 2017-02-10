c9c77127e36a7a5b804ca1034aac6118ec8afdb3
Davide Bontempelli <davide.bonte@gmail.com>
2017-02-10 15:32:00 +0100

    move methods to switch case in preparation for polimorphism

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -12,51 +12,62 @@ class GildedRose {
 			updateItem(items[i]);
 		}
 	}
-	
-	private void updateItem(Item item){
-		if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-			return;
-		}
 
-		item.sellIn -= 1;
-		
-		if (item.quality < 50) {
-			if (item.name.equals("Aged Brie")) {
-				item.quality += 1;
-				return;
-			}
+	private void updateItem(Item item) {
 
-			if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+		switch (item.name) {
+		case "Sulfuras, Hand of Ragnaros":
 
-				if (item.sellIn < 0) {
-					item.quality = 0;
-					return;
-				}
+			break;
 
-				item.quality += 1;
+		case "Aged Brie":
+			updateAgedBrie(item);
+			break;
 
-				if (item.sellIn < 10 && item.quality < 50 ) {
-					item.quality += 1;
-				}
+		case "Backstage passes to a TAFKAL80ETC concert":
+			item.sellIn -= 1;
+			updateBackstagePasses(item);
+			break;
 
-				if (item.sellIn < 5  && item.quality < 50) {
-					item.quality += 1;
-				}
-
-				return;
-			}
-			
-			if(item.quality>0){
-				item.quality -= 1;
-				
-				if(item.sellIn<0){
+		default:
+			item.sellIn -= 1;
+			if (item.quality < 50) {
+				if (item.quality > 0) {
 					item.quality -= 1;
+
+					if (item.sellIn < 0) {
+						item.quality -= 1;
+					}
 				}
 			}
-			
-			return;
-			
-			
+
+			break;
+		}
+	}
+
+	private void updateBackstagePasses(Item item) {
+		if (item.quality < 50) {
+			if (item.sellIn < 0) {
+				item.quality = 0;
+				return;
+			}
+
+			item.quality += 1;
+
+			if (item.sellIn < 10 && item.quality < 50) {
+				item.quality += 1;
+			}
+
+			if (item.sellIn < 5 && item.quality < 50) {
+				item.quality += 1;
+			}
+		}
+	}
+
+	private void updateAgedBrie(Item item) {
+		item.sellIn -= 1;
+		if (item.quality < 50) {
+			item.quality += 1;
 		}
 	}
 }
